ea9942b743a2e53906a6974af41474d1ea667372
truongvu2005 <vu67893@gmail.com>
2025-12-16 10:46:55 +0700

    Trang chi tiet san pham

---
+++ resources/views/vũ/assets/main.js
@@ -0,0 +1,200 @@
+const header = document.getElementById("header");
+const headerBigModal = document.querySelector(".header__left-modal");
+const headerLeftBtn = document.querySelector(".header__left-btn");
+const listLink = document.querySelectorAll(".header__left-modal-item");
+const listSubModal = document.querySelectorAll(".header__left-sub-modal");
+const titleUser = document.querySelector(".header__left-title");
+const headerAddress = document.querySelector(".header__address");
+const navBar = document.querySelector(".navbar");
+const headerInput = document.querySelector(".header__input");
+const close = document.querySelector(".close");
+const headerInp = document.querySelector(".header__inp");
+const headerLogin = document.querySelector(".header__login");
+const headerUp = document.querySelector(".header__up");
+const headerUser = document.querySelector(".header__user");
+const headerLeftTitleBtn = document.querySelector(".header__left-title");
+const headerLeftTitleModal = document.querySelector(
+  ".header__left-title-modal"
+);
+const bannerFormCategory = document.querySelector(".banner__form-category");
+const bannerFormCategoryModal = document.querySelector(
+  ".banner__form-category-modal"
+);
+const formAddress = document.querySelector(".banner__form-adress");
+const formAddressModal = document.querySelector(".banner__form-adress-modal");
+const formConscious = document.querySelector(
+  ".banner__form-adress-modal-conscious"
+);
+const formDistrict = document.querySelector(
+  ".banner__form-adress-modal-district"
+);
+const formSubModalConscious = document.querySelector(
+  ".banner__form-adress-sub-modal-conscious"
+);
+
+const formSubModalDistrict = document.querySelector(
+  ".banner__form-adress-sub-modal-district"
+);
+
+const iconSubModalConscious = document.querySelector(".icon-conscious");
+const iconSubModalDistrict = document.querySelector(".icon-district");
+console.log(headerLeftTitleBtn);
+console.log(listLink, listSubModal);
+function handleNavbar() {
+  if (window.scrollY > 200) {
+    if (!header.classList.contains("fixed")) {
+      header.classList.add("fixed");
+      titleUser.classList.add("none");
+      headerAddress.classList.add("block");
+      navBar.classList.add("navbarNone");
+      headerInput.classList.add("header__input-block");
+      headerUp.classList.add("custom__up");
+      headerUser.classList.add("custom__user");
+      headerLogin.classList.add("custom");
+      setTimeout(() => {
+        header.classList.add("slide-down");
+      }, 10);
+    }
+  } else {
+    header.classList.remove("slide-down");
+    header.classList.remove("fixed");
+    titleUser.classList.remove("none");
+    headerAddress.classList.remove("block");
+    headerUser.classList.remove("custom__user");
+    navBar.classList.remove("navbarNone");
+    headerUp.classList.remove("custom__up");
+    headerLogin.classList.remove("custom");
+    headerInput.classList.remove("header__input-block");
+  }
+}
+
+window.addEventListener("scroll", handleNavbar);
+
+handleNavbar();
+
+// header__left-title
+headerLeftTitleBtn.addEventListener("click", () => {
+  headerLeftTitleBtn.classList.toggle("hover");
+  headerLeftTitleModal.classList.toggle("show");
+});
+
+document.addEventListener("click", (e) => {
+  const clickInsideTitle = headerLeftTitleBtn.contains(e.target);
+  const clickInsideModal = headerLeftTitleModal.contains(e.target);
+
+  if (!clickInsideTitle && !clickInsideModal) {
+    headerLeftTitleModal.classList.remove("show");
+    headerLeftTitleBtn.classList.remove("hover");
+  }
+});
+
+// input
+headerInp.addEventListener("input", () => {
+  if (headerInp.value) {
+    close.style.display = "flex";
+  } else {
+    close.style.display = "none";
+  }
+});
+
+close.addEventListener("click", () => {
+  headerInp.value = "";
+  close.style.display = "none";
+  headerInp.focus();
+});
+
+//================================
+
+headerLeftBtn.addEventListener("mouseenter", () => {
+  headerBigModal.classList.add("open");
+});
+
+document.addEventListener("click", (e) => {
+  const clickInsideIcon = headerLeftBtn.contains(e.target);
+  const clickInsideBox = headerBigModal.contains(e.target);
+
+  if (!clickInsideIcon && !clickInsideBox) {
+    headerBigModal.classList.remove("open");
+  }
+});
+
+listLink.forEach((a) => {
+  a.addEventListener("mouseenter", () => {
+    listLink.forEach((a) => {
+      a.classList.remove("hover");
+    });
+    listSubModal.forEach((modal) => {
+      modal.classList.remove("open");
+    });
+
+    a.classList.add("hover");
+
+    const id = a.dataset.sub;
+    document.getElementById(id).classList.add("open");
+  });
+});
+
+headerBigModal.addEventListener("mouseleave", () => {
+  listLink.forEach((a) => {
+    a.classList.remove("hover");
+  });
+  listSubModal.forEach((modal) => {
+    modal.classList.remove("open");
+  });
+});
+
+// Banner
+console.log(bannerFormCategoryModal);
+
+bannerFormCategory.addEventListener("click", (e) => {
+  // bannerFormCategoryModal.classList.toggle("show");
+  if (!bannerFormCategoryModal.classList.contains("show")) {
+    bannerFormCategoryModal.classList.add("show");
+  } else {
+    bannerFormCategoryModal.classList.remove("show");
+  }
+});
+
+bannerFormCategoryModal.addEventListener("click", () => {
+  bannerFormCategoryModal.classList.remove("show");
+});
+
+document.addEventListener("click", (e) => {
+  const clickInsideIcon = bannerFormCategory.contains(e.target);
+  const clickInsideBox = bannerFormCategoryModal.contains(e.target);
+
+  if (!clickInsideIcon && !clickInsideBox) {
+    bannerFormCategoryModal.classList.remove("show");
+  }
+});
+
+//=====================================
+console.log(formAddress);
+formAddress.addEventListener("click", (e) => {
+  formAddressModal.style.display = "block";
+});
+
+document.addEventListener("click", (e) => {
+  const clickInsideIcon = formAddress.contains(e.target);
+  const clickInsideBox = formAddressModal.contains(e.target);
+
+  if (!clickInsideIcon && !clickInsideBox) {
+    formAddressModal.style.display = "none";
+  }
+});
+
+formConscious.addEventListener("click", () => {
+  formSubModalConscious.style.display = "block";
+});
+
+iconSubModalConscious.addEventListener("click", () => {
+  formSubModalConscious.style.display = "none";
+});
+
+formDistrict.addEventListener("click", () => {
+  formSubModalDistrict.style.display = "block";
+});
+
+iconSubModalDistrict.addEventListener("click", () => {
+  formSubModalDistrict.style.display = "none";
+});
